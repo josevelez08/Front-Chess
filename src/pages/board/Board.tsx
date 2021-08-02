6935a42f8b0cd1b03fc9a426dc15f9b3ca494184
josevelez08 <jose-0919@hotmail.com>
2021-08-02 13:49:26 -0500

    adding the board and the queen to chech the positions

--- src/pages/board/Board.tsx
+++ src/pages/board/Board.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import './Board.css'
+import ChessBoard from'./chess-board.jpg'
+import KingBlack from'./King_black.png'
 
 export default function Board() {
-    return <h2>Board</h2>;
+    return <h2>
+      <div id="board">
+        <img src={ChessBoard} alt="chess board"></img>
+        <div id="black_king">
+          <img src={KingBlack} alt="King blach"></img>
+        </div>
+      </div>
+    </h2>;
   } 

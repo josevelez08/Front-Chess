a333e4f42cf4deee609b8e7f6c7abf149a7ca7ce
Daniel Castaño <daniel.castano@project44.com>
2021-08-03 15:45:05 -0500

    Implement Movement Class

--- src/model/chess/movement.ts
+++ src/model/chess/movement.ts
@@ -2,17 +2,38 @@ import { ChessConstants } from './constants';
 import { Position } from './position';
 
 export class Movement {
-  private origin: Position;
-  private destination: Position;
-  private promotion: string;
+  public origin: Position = {} as Position;
+  public destination: Position = {} as Position;
+  public promotion: string = '';
 
-  constructor(str: string) {
+  constructor(
+    str: any,
+    dcol: number = 0,
+    drow: number = 0,
+    promotion: string = ChessConstants.EMPTY_BOX
+  ) {
     // 01234 56
     // XN-XN[=C]
-    this.origin = new Position(str.substring(0, 2));
-    this.destination = new Position(str.substring(3, 5));
-    if (str.length > 5) {
-      this.promotion = str.charAt(6);
-    } else this.promotion = ChessConstants.EMPTY_BOX;
+
+    if (typeof str === 'string') {
+      this.origin = new Position(str.substring(0, 2));
+      this.destination = new Position(str.substring(3, 5));
+      if (str.length > 5) {
+        this.promotion = str.charAt(6);
+      } else this.promotion = ChessConstants.EMPTY_BOX;
+    }
+
+    if (typeof str === 'object') {
+      this.origin = str;
+      this.destination = new Position(
+        this.origin.col + dcol,
+        this.origin.row + drow
+      );
+      this.promotion = promotion;
+    }
+  }
+
+  public isPromotion(): boolean {
+    return this.promotion !== ChessConstants.EMPTY_BOX;
   }
 }

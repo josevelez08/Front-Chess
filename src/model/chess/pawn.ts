61dd58f0e3e8753720bbd6b4f8cdc2c6292c9707
Daniel Castaño <daniel.castano@project44.com>
2021-08-06 16:04:24 -0500

    Implementation Piece Class

--- src/model/chess/pawn.ts
+++ src/model/chess/pawn.ts
@@ -1,1 +1,17 @@
-export class Pawn {}
+import { Board } from './board';
+import { Movement } from './movement';
+import { IPiece, Piece } from './piece';
+import { Position } from './position';
+
+export class Pawn extends Piece implements IPiece {
+  public canEat(Board: Board, position: Position): boolean {
+    return true;
+  }
+  public possibleMovements(): Array<Movement> {
+    return [];
+  }
+
+  public copy(): Piece {
+    return {} as Piece;
+  }
+}
--- src/model/chess/piece.ts
+++ src/model/chess/piece.ts
@@ -1,3 +1,14 @@
+import { Board } from './board';
+import { Movement } from './movement';
+import { Position } from './position';
+
 export class Piece {
   constructor(protected color: boolean, protected position: string) {}
+}
+
+export interface IPiece {
+  canEat: (Board: Board, position: Position) => boolean;
+
+  copy: () => Piece;
+  possibleMovements: (board: Board) => Array<Movement>;
 }

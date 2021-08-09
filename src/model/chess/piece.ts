9feb24c3b13a1dafd61a33f738ab94f3caf3900c
josevelez08 <jose-0919@hotmail.com>
2021-08-09 15:24:05 -0500

    adding mthods to piece class

--- src/model/chess/piece.ts
+++ src/model/chess/piece.ts
@@ -3,12 +3,43 @@ import { Movement } from './movement';
 import { Position } from './position';
 
 export class Piece {
-  constructor(protected color: boolean, protected position: string) {}
+  constructor(protected color: boolean, protected position: string) {
+  }
+  //return the piece's position
+  getPosition () {
+    return this.position;
+  }
+  setPosition (position2: string)
+  {
+    this.position = position2;
+  }
+  getColor() 
+  {
+    return this.color;
+  }
+  charToPiece(character: string, column: number, row: number)
+  {
+    let color = character;
+
+    if(color == color.toLocaleLowerCase())
+      { color = 'white' }
+    else 
+      { color = 'black'}
+
+    switch(character.toLocaleLowerCase()) {
+      case 'r':
+        return 'R';
+      case 'q':
+        return 'q';
+    }
+  }
 }
+
 
 export interface IPiece {
   canEat: (Board: Board, position: Position) => boolean;
 
   copy: () => Piece;
   possibleMovements: (board: Board) => Array<Movement>;
+  piezaChar: () => string;
 }
